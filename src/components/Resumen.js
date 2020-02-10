7814a0d005ce584eee6a6bc6422be601193d298e
Cesar Murillo <namiayumis@gmail.com>
2020-02-10 17:28:16 +0900

    mostrando el resumen

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,14 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { primerMayuscula } from "../helper";
+
+const ContenedorResumen = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838f;
+  color: #fff;
+  margin-top: 1rem;
+`;
 
 const Resumen = ({ datos }) => {
   // extraer de datos
@@ -6,14 +16,14 @@ const Resumen = ({ datos }) => {
 
   if (marca === "" || year === "" || plan === "") return null;
   return (
-    <>
+    <ContenedorResumen>
       <h2>Resumen de Cotización</h2>
       <ul>
-        <li>Marca: </li>
-        <li>Plan: </li>
-        <li>Año del Auto: </li>
+        <li>Marca: {primerMayuscula(marca)}</li>
+        <li>Plan: {plan}</li>
+        <li>Año del Auto: {year} </li>
       </ul>
-    </>
+    </ContenedorResumen>
   );
 };
 
--- src/helper.js
+++ src/helper.js
@@ -30,3 +30,8 @@ export function calcularMarca(marca) {
 export function obtenerPlan(plan) {
   return plan === "basico" ? 1.2 : 1.5;
 }
+
+//Muestra la primera letra mayuscula
+export function primerMayuscula(texto) {
+  return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
