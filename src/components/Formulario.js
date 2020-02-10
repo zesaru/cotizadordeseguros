8194af50cf2082c8a64cbf05689cceb84272cc00
Cesar Murillo <namiayumis@gmail.com>
2020-02-10 13:29:55 +0900

    calculando el consto basado en la marca

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { obtenerDiferenciaYear } from "../helper";
+import { obtenerDiferenciaYear, calcularMarca } from "../helper";
 
 const Campo = styled.div`
   display: flex;
@@ -89,6 +89,8 @@ const Formulario = () => {
     // Americano 15
     // Asiatico 5%
     // Europeo 30%
+    resultado = calcularMarca(marca) * resultado;
+    console.log(resultado);
 
     // Basíco aumenta 20%
     // Completo 50%
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,27 @@
+// obtiene la diferencia de años
+
 export function obtenerDiferenciaYear(year) {
   return new Date().getFullYear() - year;
+}
+
+//calcula el total a pagar segun la marca
+
+export function calcularMarca(marca) {
+  let incremento;
+
+  switch (marca) {
+    case "europeo":
+      incremento = 1.3;
+      break;
+    case "americano":
+      incremento = 1.15;
+      break;
+    case "asiatico":
+      incremento = 1.15;
+      break;
+    default:
+      break;
+  }
+
+  return incremento;
 }
