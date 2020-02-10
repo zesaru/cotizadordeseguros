b3c325d5e48d192cd2c89d92ed2077dc35fc607c
Cesar Murillo <namiayumis@gmail.com>
2020-02-10 12:31:09 +0900

    validando form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -36,12 +36,23 @@ const Boton = styled.button`
     cursor: pointer;
   }
 `;
+
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
   const [datos, guardarDatos] = useState({
     marca: "",
     year: "",
     plan: ""
   });
+  const [error, guardarError] = useState(false);
 
   // extraer los valores del state
   const { marca, year, plan } = datos;
@@ -53,8 +64,35 @@ const Formulario = () => {
       [e.target.name]: e.target.value
     });
   };
+
+  // cuando el usuario presiona submit
+  const cotizarSeguro = e => {
+    e.preventDefault();
+
+    if (marca.trim() === "" || year.trim() === "" || plan.trim() === "") {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    // obtener la diferencia de años
+
+    // por cada año hay que restar el 3%
+
+    // Americano 15
+    // Asiatico 5%
+    // Europeo 30%
+
+    // Basíco aumenta 20%
+    // Completo 50%
+
+    // Total
+  };
+
   return (
-    <form>
+    <form onSubmit={cotizarSeguro}>
+      {error ? <Error>Todos los campos son obligatorios</Error> : null}
       <Campo>
         <Label>Marca</Label>
         <Select name="marca" value={marca} onChange={obtenerInformacion}>
@@ -102,7 +140,7 @@ const Formulario = () => {
         Completo
       </Campo>
 
-      <Boton type="button">Cotizar</Boton>
+      <Boton type="submit">Cotizar</Boton>
     </form>
   );
 };
